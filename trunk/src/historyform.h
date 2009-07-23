5c64381b598620c45051e046554fd84213291bf6
amineroukh <amineroukh@b8485274-75e2-11de-94da-c3e9e1814b9e>
2009-07-23 15:48:59 +0000

    Add history ui

---
+++ trunk/src/historyform.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2008-2009 by Amine Roukh       <amineroukh@gmail.com>   *
+ *   Copyright (C) 2009 by Eduard Kalinowski      <e-h-s@arcor.de>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#ifndef HISTORYFORM_H
+#define HISTORYFORM_H
+
+#include <QDialog>
+#include "ui_historyForm.h"
+
+class History : public QDialog, private Ui::historyWidget
+{
+    Q_OBJECT
+
+public:
+    History(QWidget* parent = 0, Qt::WFlags fl = 0 );
+    ~History();
+
+public slots:
+
+protected:
+
+protected slots:
+
+};
+
+#endif
+
